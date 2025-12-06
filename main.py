763c4b082e0f3cb4ef8578791347db29f220dcb0
kehiy <me@kehiy.net>
2025-12-06 18:03:26 +0330

    feat: add assets

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2
 
 for i in range(10):
-    f_name = f"examples/{i}.png"
-    color_image = cv2.imread(f_name)
+    sf_name = f"raw/{i}.png"
+    df_name = f"processed/{i}.png"
+    color_image = cv2.imread(sf_name)
     gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
     _, binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY_INV)
-    cv2.imwrite(f_name, binary_image)
+    binary_image = cv2.resize(binary_image, (28, 28))
+    cv2.imwrite(df_name, binary_image)
